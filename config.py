d5d8acf9438f3b8aa0509e8ce24810d74f7fd823
Bryan <dbryanchick@gmail.com>
2019-02-20 21:53:30 -0700

    added images, and weather data from WeatherPy assignment

---
+++ config.py
@@ -0,0 +1,13 @@
+# Enter your API key
+
+
+#this is my open weather key
+wkey = "f51381799eb995c69b073fdfce892a3f"
+
+guardian_api = "aaaee297-61a6-46ab-ac59-cf358a72b175"
+
+
+#Ny Times api
+Nykey = "ce29c3c9c7604a7abcd9e52a2742a837"
+
+api_key ="65f967eea20d80e034e7b17430daa70f"
